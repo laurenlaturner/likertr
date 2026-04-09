fcc9d90dea11d98fc24bd7ebb3f74673852aeb55
Lauren La Turner <laturnerlauren@gmail.com>
2026-04-09 14:01:15 -0600

    regular renamed to stacked bar

--- R/plot.likertr.R
+++ R/plot.likertr.R
@@ -12,14 +12,34 @@ plot.likertr <- function(likertr, ...) {
 
 }
 
-regular_bar <- function(perc_by_question, questions) {
-  # Needs work because of possible 1-7 and 1-5 likert
-  barplot(perc_by_question,
+stacked_bar <- function(perc_by_question, questions) {
+  widths <- sapply(perc_by_question, length)
+  max_w  <- max(widths)
+  
+  aligned_data <- sapply(perc_by_question, function(x) {
+    len <- length(x)
+    if (len == max_w) return(as.numeric(x))
+    
+    left_pad <- (max_w - len) / 2
+    combined <- rep(0, max_w)
+    
+    combined[(left_pad + 1):(left_pad + len)] <- x
+    return(combined)
+  })
+
+  base_colors <- c("#D7191C", "#FDAE61", "#FFFFBF", "#ABD9E9", "#2C7BB6")
+  dynamic_cols <- colorRampPalette(base_colors)(max_w)
+
+  par(xpd = TRUE)
+
+  barplot(aligned_data,
           horiz = TRUE,
           las = 1,
           beside = FALSE,
           xlab = "Percentage (%)", 
           names.arg = questions,
-          col = c("#D7191C", "#FDAE61", "#FFFFBF", "#ABD9E9", "#2C7BB6"),
-  )
+          col = dynamic_cols,
+          border = "white")
+  
+  par(xpd = FALSE)
 }
