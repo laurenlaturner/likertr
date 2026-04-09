875f2682f8df0f0e7215af03c439654c06dbcee2
a02352588 <a02352588@aggies.usu.edu>
2026-04-09 13:51:26 -0600

    omega update

--- R/mcdonalds_omega.R
+++ R/mcdonalds_omega.R
@@ -3,20 +3,27 @@
 
 
 #' ADD DOCUMENTATION :)
-mcdonalds_omega <- function(data, loadings, empirical = TRUE) {
+mcdonalds_omega <- function(data, loadings, std = TRUE, empirical = TRUE) {
 
   # ADD SUPPORT FOR MULTIFACTOR MODELS ?
 
   numerator <- sum(loadings)^2
 
   if (empirical) {
-    denominator <- sum(cov(data))
-    return(numerator / denominator)
+    if (std) {
+      denominator <- sum(cor(data, use = "complete.obs"))
+    } else {
+      denominator <- sum(cov(data, use = "complete.obs"))
+    }
+  } else {
+    if (std) {
+      residuals <- 1 - (loadings^2)
+    } else {
+      item_vars <- apply(data, 2, var, na.rm = TRUE)
+      residuals <- item_vars - (loadings^2)
+    }
+    denominator <- sum(loadings)^2 + sum(residuals)
   }
-
-  item_vars <- apply(data, 2, var)
-  residuals <- item_vars - (loadings^2)
-  denominator <- sum(loadings)^2 + sum(residuals)
 
   numerator / denominator
 }
