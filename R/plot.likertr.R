854f5fa0b3c3ff42ef9a2d253c7df054c36c0878
Lauren La Turner <laturnerlauren@gmail.com>
2026-04-09 14:29:44 -0600

    alignment func, stacked & diverging bars

--- R/plot.likertr.R
+++ R/plot.likertr.R
@@ -12,34 +12,64 @@ plot.likertr <- function(likertr, ...) {
 
 }
 
-stacked_bar <- function(perc_by_question, questions) {
+align_likert_data <- function(perc_by_question) {
   widths <- sapply(perc_by_question, length)
   max_w  <- max(widths)
+  mid_idx <- ceiling(max_w / 2)
   
-  aligned_data <- sapply(perc_by_question, function(x) {
+  # Process each question
+  processed <- lapply(perc_by_question, function(x) {
     len <- length(x)
-    if (len == max_w) return(as.numeric(x))
-    
     left_pad <- (max_w - len) / 2
-    combined <- rep(0, max_w)
     
+    # Create the padded vector
+    combined <- rep(0, max_w)
     combined[(left_pad + 1):(left_pad + len)] <- x
-    return(combined)
+    
+    # Calculate offset (for diverging plots)
+    # sum of everything left of neutral + half of neutral
+    offset <- sum(combined[1:(mid_idx - 1)]) + (combined[mid_idx] / 2)
+    
+    return(list(vec = combined, offset = offset))
   })
-
-  base_colors <- c("#D7191C", "#FDAE61", "#FFFFBF", "#ABD9E9", "#2C7BB6")
-  dynamic_cols <- colorRampPalette(base_colors)(max_w)
-
-  par(xpd = TRUE)
-
-  barplot(aligned_data,
-          horiz = TRUE,
-          las = 1,
-          beside = FALSE,
-          xlab = "Percentage (%)", 
-          names.arg = questions,
-          col = dynamic_cols,
-          border = "white")
   
-  par(xpd = FALSE)
+  # Simplify into a matrix and a vector of offsets
+  matrix_data <- do.call(cbind, lapply(processed, `[[`, "vec"))
+  offsets <- sapply(processed, `[[`, "offset")
+  
+  return(list(
+    matrix = matrix_data, 
+    offsets = offsets, 
+    max_w = max_w,
+    mid_idx = mid_idx
+  ))
+}
+
+stacked_bar <- function(perc_by_question, questions) {
+  prep <- align_likert_data(perc_by_question)
+  
+  # Generate colors
+  cols <- colorRampPalette(c("#D7191C", "#FFFFBF", "#2C7BB6"))(prep$max_w)
+  
+  par(mar = c(5, 15, 5, 2), xpd = TRUE)
+  barplot(prep$matrix, horiz = TRUE, col = cols, names.arg = questions, 
+          las = 1, xlab = "Percentage (%)")
+}
+
+diverging_bar <- function(perc_by_question, questions) {
+  prep <- align_likert_data(perc_by_question)
+  
+  cols <- colorRampPalette(c("#D7191C", "#FFFFBF", "#2C7BB6"))(prep$max_w)
+  
+  par(mar = c(5, 15, 5, 2), xpd = TRUE)
+  barplot(prep$matrix, 
+          horiz = TRUE, 
+          col = cols, 
+          names.arg = questions, 
+          las = 1, 
+          offset = -prep$offsets,
+          xlim = c(-100, 100),
+          xlab = "Negative <--- Neutral ---> Positive")
+  
+  abline(v = 0, lty = 2, col = "gray40")
 }
