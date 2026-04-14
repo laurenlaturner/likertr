ac07ac462d5548c1814e86248a26ea288e0c721b
Lauren La Turner <laturnerlauren@gmail.com>
2026-04-14 14:46:02 -0600

    beginning of roxy documentation

--- R/likertr.R
+++ R/likertr.R
@@ -1,6 +1,22 @@
-# This is the overall wrapper function that will run everything
+#' Likertr Wrapper Function
+#' 
+#' @description
+#' 
+#' @param data
+#' @param max_val
+#' @param na_decision
+#' @param ipsatize_decision
+#' @param small_n_decision
+#' @param groups
+#' @param std
+#' @param empirical
+#' 
+#' @example
+#' 
+#' @export
 
-likertr <- function(data, max_val, na_decision = "drop", ipsatize_decision = FALSE, small_n_decision = "nothing", groups = numeric(0), std = TRUE, empirical = TRUE) {
+likertr <- function(data, max_val, na_decision = "drop", ipsatize_decision = FALSE, small_n_decision = "nothing", 
+  groups = numeric(0), std = TRUE, empirical = TRUE) {
   # Preparation and Cleaning
   clean_data <- preparation(data, na_decision, ipsatize_decision, small_n_decision)
 
