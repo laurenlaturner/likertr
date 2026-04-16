182260b6a76cd6a6c5639fbcc3d84c8a72bab364
a02352588 <a02352588@aggies.usu.edu>
2026-04-16 14:25:01 -0600

    omega documentation

--- R/mcdonalds_omega.R
+++ R/mcdonalds_omega.R
@@ -5,8 +5,15 @@
 #' @title mcdonalds_omega
 #' @description function for calculating McDonald's Omega for likert survey data
 #'
-#' DOCUMENTATION :)
+#' @param data a dataframe where each column is a likert survey question (item)
+#'     and each row is a response.
+#' @param n_fact Number of factors believed to be group factors
+#' @param flip If flip is TRUE, then items are automatically flipped to have
+#'     positive correlations on the general factor (recommended to do this
+#'     manually before)
+#' @param plot Whether or not to call omega.diagram
 #'
+#' @returns list containing omega hierarchical and omega total values
 mcdonalds_omega <- function(data, n_fact, flip, plot) {
 
   psych_omega <- psych::omega(m = data,
