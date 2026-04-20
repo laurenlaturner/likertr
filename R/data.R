332b4a63823078de7647fc2f9a037d0d0736a8ea
Lauren La Turner <laturnerlauren@gmail.com>
2026-04-20 10:02:13 -0600

    data.R to raw-data file

--- R/data.R
+++
@@ -1,9 +0,0 @@
-# data used in the vignette
-data <- read.csv("data-raw/Stress_Dataset.csv")
-
-use_data(data)
-
-# data used in the readme
-readme_data <- read.csv("data-raw/likert_sample_data.csv")
-
-use_data(readme_data)
