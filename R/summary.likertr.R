ff5e99991ae18c09b0fed2bb8325a62b31ff297b
dawson-tree <dtree03@gmail.com>
2026-04-20 15:08:39 -0600

    Fixed summary lints

--- R/summary.likertr.R
+++ R/summary.likertr.R
@@ -18,7 +18,10 @@ summary.likertr <- function(x, ...) {
   cat("================================================\n")
   cat("LIKERTR OBJECT SUMMARY REPORT\n")
   cat("================================================\n")
-  cat(paste0("This dataset contains ", n_q, " questions with ", n_obs, " total observations.\n"))
+  cat(paste0(
+    "This dataset contains ", n_q, " questions with ", n_obs,
+    " total observations.\n"
+  ))
 
   cat("================================================\n")
   cat("Exploratory Factor Analysis (EFA)\n")
@@ -29,44 +32,50 @@ summary.likertr <- function(x, ...) {
   cat("================================================\n\n")
 
   sph_p_val <- attributes(x)$pre_efa_diagnostics$sphericity$p_value
-  MSAi <- attributes(x)$pre_efa_diagnostics$kmo$MSAi
+  msai <- attributes(x)$pre_efa_diagnostics$kmo$MSAi
   user_n_fact <- attributes(x)$efa_results$user_n_fact
   n_fact <- attributes(x)$efa_results$n_fact
 
-  invalid_kmo <- MSAi < 0.6
+  invalid_kmo <- msai < 0.6
 
   if (sph_p_val > 0.05) {
-    cat(paste0("Bartlett's sphericity test resulted in a non-significant ",
-              "(p<0.05) p-value of ",
-              sph_p_val,
-              ", variables may not be correlated enough for EFA\n\n")
-        )
+    cat(paste0(
+      "Bartlett's sphericity test resulted in a non-significant ",
+      "(p<0.05) p-value of ",
+      sph_p_val,
+      ", variables may not be correlated enough for EFA\n\n"
+    ))
   } else {
-    cat(paste("Bartlett's sphericity test does not show problematic results",
-              "\n\n")
-        )
+    cat(paste(
+      "Bartlett's sphericity test does not show problematic results",
+      "\n\n"
+    ))
   }
 
 
   if (any(invalid_kmo)) {
-    invalid_kmo <- MSAi[!kmo_validity]
-    cat(paste("At least one MSAi value from the KMO test is less than 0.6 and",
-              "possibly problematic, the following feature or features share",
-              "little variance with the rest of the data:\n")
-        )
+    invalid_kmo <- msai[invalid_kmo]
+    cat(paste(
+      "At least one MSAi value from the KMO test is less than 0.6 and",
+      "possibly problematic, the following feature or features share",
+      "little variance with the rest of the data:\n"
+    ))
     print(invalid_kmo)
     cat("\n")
   } else {
-    cat(paste("The KMO test does not show problematic results",
-              "\n\n")
-        )
+    cat(paste(
+      "The KMO test does not show problematic results",
+      "\n\n"
+    ))
   }
 
 
-  cat(paste("Polychoric correlation matrix results can be viewed using the",
-            "'plot' function\n - Lots of very low coefficients in the matrix",
-            "indicate low factorability\n - Lots of very high loadings",
-            "indicate redundant variables\n\n"))
+  cat(paste(
+    "Polychoric correlation matrix results can be viewed using the",
+    "'plot' function\n - Lots of very low coefficients in the matrix",
+    "indicate low factorability\n - Lots of very high loadings",
+    "indicate redundant variables\n\n"
+  ))
 
 
   cat("================================================\n")
@@ -77,22 +86,25 @@ summary.likertr <- function(x, ...) {
   var_exp <- round(attributes(x)$efa_results$var_exp, 4)
   communality <- round(attributes(x)$efa_results$communality, 4)
   fc_matrix <- round(attributes(x)$efa_results$fc_matrix, 4)
-  RMSEA <- round(attributes(x)$efa_results$RMSEA, 4)
-  TLI <- round(attributes(x)$efa_results$TLI, 4)
-  CFI <- round(attributes(x)$efa_results$CFI, 4)
+  rmsea <- round(attributes(x)$efa_results$RMSEA, 4)
+  tli <- round(attributes(x)$efa_results$TLI, 4)
+  cfi <- round(attributes(x)$efa_results$CFI, 4)
 
 
   if (user_n_fact) {
-    cat(paste0("User-supplied number of factors (",
-               n_fact,
-               ") was used for EFA\n\n"))
+    cat(paste0(
+      "User-supplied number of factors (",
+      n_fact,
+      ") was used for EFA\n\n"
+    ))
   } else {
-    cat(paste0("No 'n' argument was given and number of factors (",
-               n_fact,
-               ") used in EFA was determined using parallel analysis\n\n",
-               "Check parallel analysis Scree plot using 'plot' function for ",
-               "more details\n\n")
-    )
+    cat(paste0(
+      "No 'n' argument was given and number of factors (",
+      n_fact,
+      ") used in EFA was determined using parallel analysis\n\n",
+      "Check parallel analysis Scree plot using 'plot' function for ",
+      "more details\n\n"
+    ))
   }
 
 
@@ -122,17 +134,20 @@ summary.likertr <- function(x, ...) {
 
   if (any(low_communality)) {
     lc_variables <- communality[low_communality]
-    cat(paste("The following variables have communality values less than 0.2,",
-              "which means that very little of their variance is explained by",
-              "the common factors and they should be considered for removal:\n")
-    )
+    cat(paste(
+      "The following variables have communality values less than 0.2,",
+      "which means that very little of their variance is explained by",
+      "the common factors and they should be considered for removal:\n"
+    ))
     print(lc_variables)
     cat("\n")
   } else {
-    cat(paste("There are no variables with a communality less than 0.2,",
-              "but it is reccommended to review the communality values that",
-              "show how much of the variance of each variable is explained by",
-              "the common factors"))
+    cat(paste(
+      "There are no variables with a communality less than 0.2,",
+      "but it is reccommended to review the communality values that",
+      "show how much of the variance of each variable is explained by",
+      "the common factors"
+    ))
   }
 
   # We want variables with a high communality that contribute strongly to the
@@ -144,32 +159,32 @@ summary.likertr <- function(x, ...) {
   cat("\n\n")
 
   cat("Measures of Fit:\n")
-  cat(paste("RMSEA:", RMSEA, "\n"))
-  if (RMSEA < 0.05) {
+  cat(paste("RMSEA:", rmsea, "\n"))
+  if (rmsea < 0.05) {
     cat("This RMSEA value indicates a good model fit")
-  } else if (RMSEA < 0.08) {
+  } else if (rmsea < 0.08) {
     cat("This RMSEA value indicates an acceptable model fit")
-  } else if (RMSEA < 0.1) {
+  } else if (rmsea < 0.1) {
     cat("This RMSEA value indicates a marginal model fit")
   } else {
     cat("This RMSEA value indicates a poor model fit")
   }
   cat("\n\n")
 
-  cat(paste("TLI:", TLI, "\n"))
-  if (TLI > 0.95) {
+  cat(paste("TLI:", tli, "\n"))
+  if (tli > 0.95) {
     cat("This TLI value indicates a good model fit")
-  } else if (TLI > 0.9) {
+  } else if (tli > 0.9) {
     cat("This TLI value indicates an acceptable model fit")
   } else {
     cat("This TLI value indicates a poor model fit")
   }
   cat("\n\n")
 
-  cat(paste("CFI:", CFI, "\n"))
-  if (CFI > 0.95) {
+  cat(paste("CFI:", cfi, "\n"))
+  if (cfi > 0.95) {
     cat("This CFI value indicates a good model fit")
-  } else if (CFI > 0.9) {
+  } else if (cfi > 0.9) {
     cat("This CFI value indicates an acceptable model fit")
   } else {
     cat("This CFI value indicates a poor model fit")
@@ -177,8 +192,10 @@ summary.likertr <- function(x, ...) {
   cat("\n\n")
 
 
-  cat(paste("Keep in mind that the interpretation of many of these statistics",
-            "will depend on the context of your analysis\n\n"))
+  cat(paste(
+    "Keep in mind that the interpretation of many of these statistics",
+    "will depend on the context of your analysis\n\n"
+  ))
 
   cat("================================================\n")
   cat("Cronbach's Alpha\n")
@@ -203,7 +220,6 @@ summary.likertr <- function(x, ...) {
     cat("Alpha after removing: \n")
 
     for (j in seq_len(items)) {
-
       if (alpha[[i]][[2]][j, 2] > alpha[[i]][[1]]) {
         cat(
           alpha[[i]][[2]][j, 1],
