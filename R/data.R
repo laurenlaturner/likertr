beafcf2e5dd528c779d7db919cebfaf9f7192d1e
Lauren La Turner <laturnerlauren@gmail.com>
2026-04-20 21:54:21 -0600

    stuff

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Sample Likert Data
+#'
+#' A description of what this data is.
+#'
+#' @format A data frame with X rows and Y variables
+"data"
+
+#' Readme Example Data
+#'
+#' Data used for the README examples.
+#'
+#' @format A data frame with X rows and Y variables
+"readme_data"
